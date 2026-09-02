c5ac9c527cb13a934e917d54c3344292a616db99
bungacinta <bungacinta02@student.ub.ac.id>
2026-09-02 13:48:04 +0700

    login 1

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -2,8 +2,9 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 
 class UserSeeder extends Seeder
 {
@@ -12,6 +13,31 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        User::updateOrCreate(
+            ['email' => 'test@example.com'],
+            [
+                'name' => 'Test User',
+                'password' => Hash::make('password123'),
+                'role' => 'SO',
+            ]
+        );
+
+        User::updateOrCreate(
+            ['email' => 'andi@perusahaan.com'],
+            [
+                'name' => 'Andi',
+                'password' => Hash::make('password123'),
+                'role' => 'SO',
+            ]
+        );
+
+        User::updateOrCreate(
+            ['email' => 'rina@perusahaan.com'],
+            [
+                'name' => 'Rina',
+                'password' => Hash::make('password123'),
+                'role' => 'SO',
+            ]
+        );
     }
 }
